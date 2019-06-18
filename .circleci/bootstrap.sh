6051bd3c6577a79459105ecb8c6b6a457da93e44
Jeremy Gustie <jeremy@carbonrelay.com>
2019-06-18 17:09:08 -0400

    Try capturing configurations as part of the build

--- .circleci/bootstrap.sh
+++ .circleci/bootstrap.sh
@@ -4,6 +4,11 @@ set -euo pipefail
 echo "Installing make"
 apt-get update -yq && apt-get install -yq make
 
+echo "Installing Kustomize"
+curl -L https://github.com/kubernetes-sigs/kustomize/releases/download/v1.0.11/kustomize_1.0.11_linux_amd64 > kustomize
+chmod +x kustomize
+sudo mv kustomize /usr/bin/local
+
 function defineEnvvar {
     echo "  $1=$2"
     echo "export $1=\"$2\"" >> $BASH_ENV
