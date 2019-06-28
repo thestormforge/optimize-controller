13dd0202ad2d5f827b5d728f13a4484e5ed01d89
Jeremy Gustie <jeremy@carbonrelay.com>
2019-06-28 11:07:19 -0400

    Fix version in CLI

--- pkg/cordeliactl/cordeliactl.go
+++ pkg/cordeliactl/cordeliactl.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/gramLabs/cordelia/pkg/version"
 	"github.com/spf13/cobra"
 )
 
@@ -13,18 +14,14 @@ var rootCmd = &cobra.Command{
 
 func init() {
 	rootCmd.Run = rootCmd.HelpFunc()
-	rootCmd.Version = Version()
+	rootCmd.Version = version.GetVersion()
 	rootCmd.AddCommand(newInitCommand())
 	rootCmd.AddCommand(newResetCommand())
 
 	// TODO Add additional commands to the client
 	// create experiment [--remote-only]
 
-}
 
-// Version returns the current version of the application
-func Version() string {
-	return "v1.0.0-alpha.1"
 }
 
 // Execute runs the application
