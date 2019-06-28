f409f5011846f17a47e0e2e49bd21daebbdf5a3e
Jeremy Gustie <jeremy@carbonrelay.com>
2019-06-28 11:31:57 -0400

    Adjust CircleCI for building the CLI

--- .circleci/bootstrap.sh
+++ .circleci/bootstrap.sh
@@ -1,18 +1,35 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "Installing make"
-apt-get update -yq && apt-get install -yq make
-
-echo "Installing Kustomize"
-curl -L https://github.com/kubernetes-sigs/kustomize/releases/download/v2.1.0/kustomize_2.1.0_linux_amd64 > kustomize
-chmod +x kustomize
-mv kustomize /usr/local/bin
-
+KUBEBUILDER_VERSION=1.0.8
+KUSTOMIZE_VERSION=2.1.0
 function defineEnvvar {
     echo "  $1=$2"
     echo "export $1=\"$2\"" >> $BASH_ENV
 }
+
+
+echo "Installing Kubebuilder"
+curl -L -O https://github.com/kubernetes-sigs/kubebuilder/releases/download/v${KUBEBUILDER_VERSION}/kubebuilder_${KUBEBUILDER_VERSION}_linux_amd64.tar.gz
+tar -zxvf kubebuilder_${KUBEBUILDER_VERSION}_linux_amd64.tar.gz
+mv kubebuilder_${KUBEBUILDER_VERSION}_linux_amd64 kubebuilder && sudo mv kubebuilder /usr/local/
+export PATH=$PATH:/usr/local/kubebuilder/bin
+
+
+echo "Installing Kustomize"
+curl -L https://github.com/kubernetes-sigs/kustomize/releases/download/v${KUSTOMIZE_VERSION}/kustomize_${KUSTOMIZE_VERSION}_linux_amd64 > kustomize
+chmod +x kustomize
+mv kustomize /usr/local/bin
+
+
+echo "Installing Google Cloud SDK"
+curl https://sdk.cloud.google.com | bash -s -- --disable-prompts
+export PATH=$PATH:~/google-cloud-sdk/bin
+echo $GCLOUD_SERVICE_KEY | gcloud auth activate-service-account --key-file=-
+gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
+gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
+gcloud --quiet auth configure-docker
+
 
 echo "Using environment variables from bootstrap script"
 if [[ -n "${CIRCLE_TAG:-}" ]]; then
