5b6002ed2441d331f5f3b1102dae02b2f087597d
Jeremy Gustie <jeremy@carbonrelay.com>
2019-07-30 10:31:40 -0400

    Fix client configuration path

--- pkg/redskyctl/cmd/setup/bootstrap.go
+++ pkg/redskyctl/cmd/setup/bootstrap.go
@@ -255,7 +255,7 @@ func NewBootstrapInitConfig(o *SetupOptions, clientConfig *api.Config) (*Bootstr
 								VolumeMounts: []corev1.VolumeMount{
 									{
 										Name:      "client-config",
-										MountPath: "/redsky/client/client.yaml",
+										MountPath: "/redskyops/install/client.yaml",
 										SubPath:   "client.yaml",
 									},
 								},
