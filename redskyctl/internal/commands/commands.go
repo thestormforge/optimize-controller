f02f22d681057f267e4ac5afbec2542df2d35840
Jeremy Gustie <jeremy@carbonrelay.com>
2020-01-08 15:37:55 -0500

    Move the login command to its own package

--- redskyctl/internal/commands/commands.go
+++ redskyctl/internal/commands/commands.go
@@ -32,6 +32,7 @@ import (
 	"github.com/redskyops/k8s-experiment/pkg/redskyctl/cmd/setup"
 	"github.com/redskyops/k8s-experiment/pkg/redskyctl/cmd/suggest"
 	"github.com/redskyops/k8s-experiment/pkg/redskyctl/util"
+	"github.com/redskyops/k8s-experiment/redskyctl/internal/commands/login"
 	"github.com/spf13/cobra"
 )
 
@@ -61,7 +62,7 @@ func NewRedskyctlCommand(in io.Reader, out, err io.Writer) *cobra.Command {
 	rootCmd.AddCommand(docs.NewDocsCommand(ioStreams))
 	rootCmd.AddCommand(cmd.NewVersionCommand(f, ioStreams))
 
-	rootCmd.AddCommand(setup.NewLoginCommand(f, ioStreams))
+	rootCmd.AddCommand(login.NewLoginCommand(f, ioStreams))
 	rootCmd.AddCommand(setup.NewInitCommand(f, ioStreams))
 	rootCmd.AddCommand(setup.NewResetCommand(f, ioStreams))
 	rootCmd.AddCommand(setup.NewAuthorizeCommand(f, ioStreams))
