fd78b4a84f3fc5cbcfb8a140ad9fa8b2dd0f3e8c
Jeremy Gustie <jeremy@carbonrelay.com>
2020-02-03 13:52:08 -0500

    Start splitting up configuration

--- redskyapi/client.go
+++ redskyapi/client.go
@@ -22,11 +22,39 @@ import (
 	"net/http"
 	"net/url"
 	"time"
+
+	"github.com/redskyops/k8s-experiment/redskyapi/config"
 )
+
+// Config exposes the information for configuring a Red Sky Client
+type Config interface {
+	// ExperimentsURL returns a URL to the experiments API
+	ExperimentsURL(path string) (*url.URL, error)
+
+	// Authorize returns a transport that applies the authorization defined by this configuration. The
+	// supplied context is used for any additional requests necessary to perform authentication. If this
+	// configuration does not define any authorization details, the supplied transport may be returned
+	// directly.
+	Authorize(ctx context.Context, transport http.RoundTripper) (http.RoundTripper, error)
+
+	// Marshal will write the data out
+	Marshal() ([]byte, error)
+	// TODO This should be in the redskyctl configuration interface
+}
 
 type Client interface {
 	URL(endpoint string) *url.URL
 	Do(context.Context, *http.Request) (*http.Response, []byte, error)
+}
+
+// TODO Get rid of this...
+// DefaultConfig loads the mostly commonly used configuration
+func DefaultConfig() (Config, error) {
+	cc := &config.ClientConfig{}
+	if err := cc.Load(); err != nil {
+		return nil, err
+	}
+	return cc, nil
 }
 
 // NewClient returns a new client for accessing Red Sky APIs; the supplied context is used for authentication/authorization
