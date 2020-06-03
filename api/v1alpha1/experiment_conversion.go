328d33cbc47d572424263ecccb500555276feb30
Jeremy Gustie <jeremy@carbonrelay.com>
2020-06-03 11:16:11 -0400

    Add comments to v1alpha1 conversion code

--- api/v1alpha1/experiment_conversion.go
+++ api/v1alpha1/experiment_conversion.go
@@ -18,11 +18,13 @@ package v1alpha1
 
 import (
 	"github.com/redskyops/redskyops-controller/api/v1beta1"
-	conv "k8s.io/apimachinery/pkg/conversion"
-	"sigs.k8s.io/controller-runtime/pkg/conversion"
+	"k8s.io/apimachinery/pkg/conversion"
+	conv "sigs.k8s.io/controller-runtime/pkg/conversion"
 )
 
-func (in *Experiment) ConvertTo(hub conversion.Hub) error {
+var _ conv.Convertible = &Experiment{}
+
+func (in *Experiment) ConvertTo(hub conv.Hub) error {
 	s, err := SchemeBuilder.Build()
 	if err != nil {
 		return err
@@ -30,7 +32,7 @@ func (in *Experiment) ConvertTo(hub conversion.Hub) error {
 	return s.Convert(in, hub.(*v1beta1.Experiment), nil)
 }
 
-func (in *Experiment) ConvertFrom(hub conversion.Hub) error {
+func (in *Experiment) ConvertFrom(hub conv.Hub) error {
 	s, err := SchemeBuilder.Build()
 	if err != nil {
 		return err
@@ -38,16 +40,22 @@ func (in *Experiment) ConvertFrom(hub conversion.Hub) error {
 	return s.Convert(hub.(*v1beta1.Experiment), in, nil)
 }
 
-func Convert_v1alpha1_ExperimentSpec_To_v1beta1_ExperimentSpec(in *ExperimentSpec, out *v1beta1.ExperimentSpec, s conv.Scope) error {
+func Convert_v1alpha1_ExperimentSpec_To_v1beta1_ExperimentSpec(in *ExperimentSpec, out *v1beta1.ExperimentSpec, s conversion.Scope) error {
+	// Rename `Template` to `TrialTemplate`
 	if err := Convert_v1alpha1_TrialTemplateSpec_To_v1beta1_TrialTemplateSpec(&in.Template, &out.TrialTemplate, s); err != nil {
 		return err
 	}
+
+	// Continue
 	return autoConvert_v1alpha1_ExperimentSpec_To_v1beta1_ExperimentSpec(in, out, s)
 }
 
-func Convert_v1beta1_ExperimentSpec_To_v1alpha1_ExperimentSpec(in *v1beta1.ExperimentSpec, out *ExperimentSpec, s conv.Scope) error {
+func Convert_v1beta1_ExperimentSpec_To_v1alpha1_ExperimentSpec(in *v1beta1.ExperimentSpec, out *ExperimentSpec, s conversion.Scope) error {
+	// Rename `TrialTemplate` to `Template`
 	if err := Convert_v1beta1_TrialTemplateSpec_To_v1alpha1_TrialTemplateSpec(&in.TrialTemplate, &out.Template, s); err != nil {
 		return err
 	}
+
+	// Continue
 	return autoConvert_v1beta1_ExperimentSpec_To_v1alpha1_ExperimentSpec(in, out, s)
 }
--- api/v1alpha1/trial_conversion.go
+++ api/v1alpha1/trial_conversion.go
@@ -18,11 +18,13 @@ package v1alpha1
 
 import (
 	"github.com/redskyops/redskyops-controller/api/v1beta1"
-	conv "k8s.io/apimachinery/pkg/conversion"
-	"sigs.k8s.io/controller-runtime/pkg/conversion"
+	"k8s.io/apimachinery/pkg/conversion"
+	conv "sigs.k8s.io/controller-runtime/pkg/conversion"
 )
 
-func (in *Trial) ConvertTo(hub conversion.Hub) error {
+var _ conv.Convertible = &Trial{}
+
+func (in *Trial) ConvertTo(hub conv.Hub) error {
 	s, err := SchemeBuilder.Build()
 	if err != nil {
 		return err
@@ -30,7 +32,7 @@ func (in *Trial) ConvertTo(hub conversion.Hub) error {
 	return s.Convert(in, hub.(*v1beta1.Trial), nil)
 }
 
-func (in *Trial) ConvertFrom(hub conversion.Hub) error {
+func (in *Trial) ConvertFrom(hub conv.Hub) error {
 	s, err := SchemeBuilder.Build()
 	if err != nil {
 		return err
@@ -38,7 +40,8 @@ func (in *Trial) ConvertFrom(hub conversion.Hub) error {
 	return s.Convert(hub.(*v1beta1.Trial), in, nil)
 }
 
-func Convert_v1alpha1_Trial_To_v1beta1_Trial(in *Trial, out *v1beta1.Trial, s conv.Scope) error {
+func Convert_v1alpha1_Trial_To_v1beta1_Trial(in *Trial, out *v1beta1.Trial, s conversion.Scope) error {
+	// Move `Spec.PatchOperations` to `Status.PatchOperations`
 	if in.Spec.PatchOperations != nil {
 		in, out := &in.Spec.PatchOperations, &out.Status.PatchOperations
 		*out = make([]v1beta1.PatchOperation, len(*in))
@@ -51,6 +54,7 @@ func Convert_v1alpha1_Trial_To_v1beta1_Trial(in *Trial, out *v1beta1.Trial, s co
 		out.Status.PatchOperations = nil
 	}
 
+	// Move `Spec.ReadinessChecks` to `Status.ReadinessChecks`
 	if in.Spec.ReadinessChecks != nil {
 		in, out := &in.Spec.ReadinessChecks, &out.Status.ReadinessChecks
 		*out = make([]v1beta1.ReadinessCheck, len(*in))
@@ -63,10 +67,12 @@ func Convert_v1alpha1_Trial_To_v1beta1_Trial(in *Trial, out *v1beta1.Trial, s co
 		out.Status.ReadinessChecks = nil
 	}
 
+	// Continue
 	return autoConvert_v1alpha1_Trial_To_v1beta1_Trial(in, out, s)
 }
 
-func Convert_v1beta1_Trial_To_v1alpha1_Trial(in *v1beta1.Trial, out *Trial, s conv.Scope) error {
+func Convert_v1beta1_Trial_To_v1alpha1_Trial(in *v1beta1.Trial, out *Trial, s conversion.Scope) error {
+	// Move `Status.PatchOperations` to `Spec.PatchOperations`
 	if in.Status.PatchOperations != nil {
 		in, out := &in.Status.PatchOperations, &out.Spec.PatchOperations
 		*out = make([]PatchOperation, len(*in))
@@ -79,6 +85,7 @@ func Convert_v1beta1_Trial_To_v1alpha1_Trial(in *v1beta1.Trial, out *Trial, s co
 		out.Spec.PatchOperations = nil
 	}
 
+	// Move `Status.ReadinessChecks` to `Spec.ReadinessChecks`
 	if in.Status.ReadinessChecks != nil {
 		in, out := &in.Status.ReadinessChecks, &out.Spec.ReadinessChecks
 		*out = make([]ReadinessCheck, len(*in))
@@ -91,21 +98,29 @@ func Convert_v1beta1_Trial_To_v1alpha1_Trial(in *v1beta1.Trial, out *Trial, s co
 		out.Spec.ReadinessChecks = nil
 	}
 
+	// Continue
 	return autoConvert_v1beta1_Trial_To_v1alpha1_Trial(in, out, s)
 }
 
-func Convert_v1alpha1_TrialSpec_To_v1beta1_TrialSpec(in *TrialSpec, out *v1beta1.TrialSpec, s conv.Scope) error {
+func Convert_v1alpha1_TrialSpec_To_v1beta1_TrialSpec(in *TrialSpec, out *v1beta1.TrialSpec, s conversion.Scope) error {
+	// Rename `Template` to `JobTemplate`
 	out.JobTemplate = in.Template
 
+	// Continue
 	return autoConvert_v1alpha1_TrialSpec_To_v1beta1_TrialSpec(in, out, s)
 }
 
-func Convert_v1beta1_TrialSpec_To_v1alpha1_TrialSpec(in *v1beta1.TrialSpec, out *TrialSpec, s conv.Scope) error {
+func Convert_v1beta1_TrialSpec_To_v1alpha1_TrialSpec(in *v1beta1.TrialSpec, out *TrialSpec, s conversion.Scope) error {
+	// Rename `JobTemplate` to `Template`
 	out.Template = in.JobTemplate
 
+	// Continue
 	return autoConvert_v1beta1_TrialSpec_To_v1alpha1_TrialSpec(in, out, s)
 }
 
-func Convert_v1beta1_TrialStatus_To_v1alpha1_TrialStatus(in *v1beta1.TrialStatus, out *TrialStatus, s conv.Scope) error {
+func Convert_v1beta1_TrialStatus_To_v1alpha1_TrialStatus(in *v1beta1.TrialStatus, out *TrialStatus, s conversion.Scope) error {
+	// NOTE: Generation skips this function, but we handle the incompatible change in the `Trial` conversion
+
+	// Continue
 	return autoConvert_v1beta1_TrialStatus_To_v1alpha1_TrialStatus(in, out, s)
 }
