35ccd29bc87d635300e906f375bc7815b5de61bd
Jeremy Gustie <jeremy@carbonrelay.com>
2020-06-11 17:32:38 -0400

    Add code signing and notarization scripts

---
+++ hack/codesign.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+set -eu
+
+# Code Signing
+# ============
+# This script facilitates code signing of binaries for macOS. This script conditionally signs a binary
+# under the following conditions:
+# 1. The binary path contains "darwin" (this prevents duplicating GoReleaser build configurations)
+# 2. The `security` and `codesign` programs are available (this allows the script to be run on Linux)
+# 3. The P12 key pair used as the signing identity is available (allows the script to be run on non-release builds)
+
+# Verify, but exit normally to ensure the conditional nature of the checks
+FILE="${1:?missing file argument}"
+[ "$(basename "$(dirname "$FILE")")" == "redskyctl_darwin_amd64" ] || { echo >&2 "skipping code signing on file '$1'"; exit; }
+command -v security >/dev/null 2>&1 || { echo >&2 "skipping code signing, security not present"; exit; }
+command -v codesign >/dev/null 2>&1 || { echo >&2 "skipping code signing, codesign not present"; exit; }
+[ -n "${AC_IDENTITY_P12:-}" ] || { echo >&2 "skipping code signing, no signing identity"; exit; }
+
+# Create a temporary location for the keychain
+AC_IDENTITY_PASSPHRASE="${AC_IDENTITY_PASSPHRASE:-$AC_PASSWORD}"
+KEYCHAIN_PASSWORD="$(openssl rand -base64 30)"
+KEYCHAIN="$(mktemp -d)/codesigning.keychain-db"
+removeKeychain() {
+  rm -rf "$(dirname "$KEYCHAIN")"
+}
+trap removeKeychain EXIT
+
+# Create an unlocked keychain for signing
+security create-keychain -p "$KEYCHAIN_PASSWORD" "$KEYCHAIN"
+security set-keychain-settings "$KEYCHAIN" # Unset keychain settings (disable lock)
+security unlock-keychain -p "$KEYCHAIN_PASSWORD" "$KEYCHAIN"
+
+# Import the signing identity to the temporary keychain
+security import <(printenv AC_IDENTITY_P12 | base64 -D) -f pkcs12 -k "$KEYCHAIN" -P "$AC_IDENTITY_PASSPHRASE" -T "$(command -v codesign)" >/dev/null
+IDENTITY="$(security find-identity -v -p "codesigning" "$KEYCHAIN" | grep "1)" | awk '{print $2}')"
+
+# Update the keychain search list so codesign can use it
+security list-keychains -d user -s "$KEYCHAIN"
+restoreKeychain() {
+  security list-keychains -d user -s login.keychain
+}
+trap restoreKeychain EXIT
+
+# Actually sign the supplied file
+codesign --keychain "$KEYCHAIN" --sign "$IDENTITY" --force --verbose --timestamp --options "runtime" "$FILE"
---
+++ hack/notarize.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+set -eu
+
+# Notarization
+# ============
+# This script uploads binaries to Apple to get the code signatures trusted. Like the codesign script,
+# it only makes sense to do this conditionally.
+
+FILE="${1:?missing file argument}"
+NAME="$(basename "$FILE" ".tar.gz")"
+[ "$NAME" == "redskyctl-darwin-amd64" ] || { echo >&2 "skipping notarization for file=[$1]"; exit; }
+command -v ditto >/dev/null 2>&1 || { echo >&2 "skipping notarization, ditto not present"; exit; }
+command -v xcrun >/dev/null 2>&1 || { echo >&2 "skipping notarization, xcrun not present"; exit; }
+command -v jq >/dev/null 2>&1 || { echo >&2 "skipping notarization, jq not present"; exit; }
+[ -n "${AC_USERNAME:-}" ] || { echo >&2 "skipping notarization, no credentials"; exit; }
+[ -n "${AC_PASSWORD:-}" ] || { echo >&2 "skipping notarization, no credentials"; exit; }
+
+# Create a temporary location to perform notarization
+WORKDIR="$(mktemp -d)"
+function removeWorkdir()
+{
+  rm -rf "$WORKDIR"
+}
+trap removeWorkdir EXIT
+
+# Re-archive as a Zip
+mkdir "$WORKDIR/$NAME"
+tar -xf "$FILE" -C "$WORKDIR/$NAME"
+ditto -c -k "$WORKDIR/$NAME" "$WORKDIR/$NAME.zip"
+
+# Helper functions
+doNotarizeApp() {
+  xcrun altool --notarize-app --file "$1" --primary-bundle-id "dev.redskyops.redskyctl" \
+    -u "$AC_USERNAME" -p "@env:AC_PASSWORD" --output-format json | jq '."notarization-upload"'
+}
+doNotarizeInfo() {
+  xcrun altool --notarization-info "$1" \
+    -u "$AC_USERNAME" -p "@env:AC_PASSWORD" --output-format json | jq '."notarization-info"'
+}
+
+# Submit the Zip file for notarization, retain the request identifier
+REQUEST_UUID="$(doNotarizeApp "$WORKDIR/$NAME.zip" | jq -r '.RequestUUID')"
+echo >&2 "notarization request submitted id=$REQUEST_UUID"
+
+# Wait for a result
+SLEEP_TIME=10
+while true; do
+  sleep $SLEEP_TIME
+  INFO=$(doNotarizeInfo "$REQUEST_UUID")
+  case "$(echo "$INFO" | jq -r '.Status')" in
+    "in progress")
+      SLEEP_TIME=5
+      ;;
+    "success")
+      exit
+      ;;
+    "invalid")
+      LOG_FILE_URL="$(echo "$INFO" | jq -r '.LogFileURL')"
+      LOG="$(curl --silent "$LOG_FILE_URL")"
+      echo >&2 "notarization failed: $(echo "$LOG" | jq -r '.statusSummary')"
+      echo >&2 "more details: $LOG_FILE_URL"
+      exit 1
+      ;;
+  esac
+done
