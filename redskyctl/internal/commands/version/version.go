62c40118ca3ab1a4febcac859ab399b30a022ca5
Jeremy Gustie <jeremy@carbonrelay.com>
2020-06-26 10:17:17 -0400

    Do not abbreviate image as img in flags
    Also, why does autocorrect change "img" to "omg"?

--- redskyctl/internal/commands/version/version.go
+++ redskyctl/internal/commands/version/version.go
@@ -80,8 +80,8 @@ func NewCommand(o *Options) *cobra.Command {
 		RunE: commander.WithContextE(o.version),
 	}
 
-	cmd.Flags().BoolVar(&o.ShowSetupToolsImage, "setuptools-img", false, "Print only the name of the setuptools image.")
-	cmd.Flags().BoolVar(&o.ShowControllerImage, "controller-img", false, "Print only the name of the controller image.")
+	cmd.Flags().BoolVar(&o.ShowSetupToolsImage, "setuptools-image", false, "Print only the name of the setuptools image.")
+	cmd.Flags().BoolVar(&o.ShowControllerImage, "controller-image", false, "Print only the name of the controller image.")
 	cmd.Flags().BoolVar(&o.Debug, "debug", o.Debug, "Display debugging information.")
 
 	commander.ExitOnError(cmd)
