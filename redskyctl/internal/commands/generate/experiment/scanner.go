44eb3d2f0defbde82343f09540e3a17bbd4a5633
Jeremy Gustie <jeremy@carbonrelay.com>
2020-10-26 13:32:29 -0400

    Generate Prometheus setup RBAC

--- redskyctl/internal/commands/generate/experiment/scanner.go
+++ redskyctl/internal/commands/generate/experiment/scanner.go
@@ -20,6 +20,8 @@ import (
 	redskyv1beta1 "github.com/redskyops/redskyops-controller/api/v1beta1"
 	"github.com/redskyops/redskyops-controller/internal/setup"
 	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/kustomize/api/filesys"
 	"sigs.k8s.io/kustomize/api/konfig"
@@ -108,6 +110,26 @@ func findOrAddExperiment(list *corev1.List) *redskyv1beta1.Experiment {
 	return exp
 }
 
+// ensureSetupServiceAccount ensures that we are using an explicit service account for setup tasks.
+func ensureSetupServiceAccount(list *corev1.List) {
+	// Return if we see an explicit service account name
+	exp := findOrAddExperiment(list)
+	saName := &exp.Spec.TrialTemplate.Spec.SetupServiceAccountName
+	if *saName != "" {
+		return
+	}
+	*saName = "redsky-setup"
+
+	// Add the actual service account to the list
+	list.Items = append(list.Items,
+		runtime.RawExtension{Object: &corev1.ServiceAccount{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: *saName,
+			},
+		}},
+	)
+}
+
 // ensurePrometheus adds Prometheus configuration to the supplied list.
 func ensurePrometheus(list *corev1.List) {
 	// Return if we see the Prometheus setup task
@@ -124,6 +146,66 @@ func ensurePrometheus(list *corev1.List) {
 		Name: "monitoring",
 		Args: []string{"prometheus", "$(MODE)"},
 	})
+
+	// Ensure there is an explicit service account configured
+	ensureSetupServiceAccount(list)
+
+	// Append the cluster role and binding for Prometheus
+	list.Items = append(list.Items,
+		runtime.RawExtension{Object: &rbacv1.ClusterRole{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "redsky-prometheus",
+			},
+			Rules: []rbacv1.PolicyRule{
+				// Required to manage the Prometheus resources in the setup task
+				// TODO It's unclear why this isn't just create/delete on all six types
+				{
+					Verbs:     []string{"get", "create", "delete"},
+					APIGroups: []string{"rbac.authorization.k8s.io"},
+					Resources: []string{"clusterroles", "clusterrolebindings"},
+				},
+				{
+					Verbs:     []string{"get", "create", "update"},
+					APIGroups: []string{""},
+					Resources: []string{"serviceaccounts", "services", "configmaps"},
+				},
+				{
+					Verbs:     []string{"get", "create", "delete", "patch"},
+					APIGroups: []string{"apps"},
+					Resources: []string{"deployments"},
+				},
+
+				// Permissions we need to delegate to Prometheus runtime (prometheus-server-rbac.yaml)
+				{
+					Verbs:     []string{"list", "watch", "get"},
+					APIGroups: []string{""},
+					Resources: []string{"nodes", "nodes/metrics", "nodes/proxy", "services"},
+				},
+				{
+					Verbs:     []string{"list", "watch"},
+					APIGroups: []string{""},
+					Resources: []string{"pods"},
+				},
+			},
+		}},
+		runtime.RawExtension{Object: &rbacv1.ClusterRoleBinding{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "redsky-setup-prometheus",
+			},
+			RoleRef: rbacv1.RoleRef{
+				APIGroup: "rbac.authorization.k8s.io",
+				Kind:     "ClusterRole",
+				Name:     "redsky-prometheus",
+			},
+			Subjects: []rbacv1.Subject{
+				{
+					Kind: "ServiceAccount",
+					Name: exp.Spec.TrialTemplate.Spec.SetupServiceAccountName,
+				},
+			},
+		}},
+	)
+
 }
 
 // kustomizationFileSystem is a wrapper around a real file system that injects a Kustomization at
