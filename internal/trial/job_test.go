b199ae80a05db50876e33d7d7e223340520bf449
Brad Beam <brad.beam@carbonrelay.com>
2020-11-11 20:47:27 -0600

    chore: Add test for NewJob
    Simple/basic test to make sure the job is getting created properly.
    
    Signed-off-by: Brad Beam <brad.beam@carbonrelay.com>

---
+++ internal/trial/job_test.go
@@ -0,0 +1,101 @@
+/*
+Copyright 2020 GramLabs, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package trial
+
+import (
+	"fmt"
+	"testing"
+
+	redskyv1beta1 "github.com/redskyops/redskyops-controller/api/v1beta1"
+	"github.com/stretchr/testify/assert"
+	batchv1 "k8s.io/api/batch/v1"
+	batchv1beta1 "k8s.io/api/batch/v1beta1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestNewJob(t *testing.T) {
+	testCases := []struct {
+		desc               string
+		trial              *redskyv1beta1.Trial
+		expectedContainers int
+	}{
+		{
+			desc: "no containers",
+			trial: &redskyv1beta1.Trial{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "default",
+					Namespace: "default",
+				},
+				Spec: redskyv1beta1.TrialSpec{
+					JobTemplate: &batchv1beta1.JobTemplateSpec{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      "trial-job",
+							Namespace: "default",
+						},
+						Spec: batchv1.JobSpec{},
+					},
+				},
+			},
+			expectedContainers: 1,
+		},
+		{
+			desc: "two containers",
+			trial: &redskyv1beta1.Trial{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "default",
+					Namespace: "default",
+				},
+				Spec: redskyv1beta1.TrialSpec{
+					JobTemplate: &batchv1beta1.JobTemplateSpec{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      "trial-job",
+							Namespace: "default",
+						},
+						Spec: batchv1.JobSpec{
+							Template: corev1.PodTemplateSpec{
+								Spec: corev1.PodSpec{
+									Containers: []corev1.Container{
+										{
+											Name:    "trial-run-1",
+											Image:   "busybox",
+											Command: []string{"/bin/sh"},
+										},
+										{
+											Name:    "trial-run-2",
+											Image:   "busybox",
+											Command: []string{"/bin/sh"},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedContainers: 2,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("%q", tc.desc), func(t *testing.T) {
+			job := NewJob(tc.trial)
+			assert.NotNil(t, job)
+			assert.Equal(t, len(job.Spec.Template.Spec.Containers), tc.expectedContainers)
+		})
+	}
+}
