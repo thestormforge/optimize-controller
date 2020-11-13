506cb538532948e118c673dca8a7cd4081697746
Brad Beam <brad.beam@carbonrelay.com>
2020-11-12 22:15:27 -0600

    feat: Add tests for patch application
    Signed-off-by: Brad Beam <brad.beam@carbonrelay.com>

--- redskyctl/internal/commands/patch/patch_test.go
+++ redskyctl/internal/commands/patch/patch_test.go
@@ -20,7 +20,9 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"log"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/redskyops/redskyops-controller/redskyctl/internal/commander"
@@ -30,7 +32,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestPatch(t *testing.T) {
+func TestPatchExperiment(t *testing.T) {
 	_, expBytes, expFile := createTempExperimentFile(t)
 	defer os.Remove(expFile.Name())
 
@@ -100,6 +102,98 @@ func TestPatch(t *testing.T) {
 				cmd.SetArgs(tc.args)
 			}
 
+			err := cmd.Execute()
+			require.NoError(t, err)
+
+			cpu := wannabeTrial.TrialAssignments.Assignments[0]
+			mem := wannabeTrial.TrialAssignments.Assignments[1]
+			assert.Contains(t, b.String(), fmt.Sprintf("%s: %sm", cpu.ParameterName, cpu.Value.String()))
+			assert.Contains(t, b.String(), fmt.Sprintf("%s: %sMi", mem.ParameterName, mem.Value.String()))
+		})
+	}
+}
+
+func TestPatchApplication(t *testing.T) {
+	// All of these files get created in the same tempdir ( neat-o )
+	// so we can 'cheat' kustomize/krusty by passing in basename(manifests)
+	// to use the relative path and not have to go through wonky hoops
+	_, _, expFile := createTempExperimentFile(t)
+	defer os.Remove(expFile.Name())
+
+	manifestFile := createTempManifests(t)
+	defer os.Remove(manifestFile.Name())
+
+	_, b, appFile := createTempApplication(t, filepath.Base(manifestFile.Name()))
+	defer os.Remove(appFile.Name())
+	fmt.Println(string(b))
+
+	testCases := []struct {
+		desc  string
+		args  []string
+		stdin io.Reader
+	}{
+		{
+			desc: "exp file manifest file",
+			args: []string{
+				"--file", appFile.Name(),
+				"--trialname", "sampleApplication-how-do-you-make-a-tissue-dance-put-a-little-boogie-in-it-1234",
+			},
+		},
+		/*
+			{
+				desc: "exp stdin manifest file",
+				args: []string{
+					"--file", "-",
+					"--file", manifestFile.Name(),
+					"--trialname", "sampleExperiment-1234",
+				},
+				stdin: bytes.NewReader(expBytes),
+			},
+			{
+				desc: "exp file manifest stdin",
+				args: []string{
+					"--file", expFile.Name(),
+					"--file", "-",
+					"--trialname", "sampleExperiment-1234",
+				},
+				stdin: bytes.NewReader(pgDeployment),
+			},
+			{
+				desc: "exp stdin manifest stdin",
+				args: []string{
+					"--file", "-",
+					"--trialname", "sampleExperiment-1234",
+				},
+				stdin: bytes.NewReader(append(expBytes, pgDeployment...)),
+			},
+		*/
+	}
+
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("%q", tc.desc), func(t *testing.T) {
+			cfg := &config.RedSkyConfig{}
+			opts := &patch.Options{Config: cfg}
+			opts.ExperimentsAPI = &fakeRedSkyServer{}
+			cmd := patch.NewCommand(opts)
+			commander.ConfigGlobals(cfg, cmd)
+
+			// setup output
+			var b bytes.Buffer
+			cmd.SetOut(&b)
+
+			// setup input
+			if tc.stdin != nil {
+				cmd.SetIn(tc.stdin)
+			}
+
+			// set command args
+			if len(tc.args) > 0 {
+				cmd.SetArgs(tc.args)
+			}
+			log.Println(appFile.Name())
+			log.Println(manifestFile.Name())
+
+			//time.Sleep(1 * time.Minute)
 			err := cmd.Execute()
 			require.NoError(t, err)
 
