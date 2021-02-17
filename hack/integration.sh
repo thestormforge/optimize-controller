7ea1b4c6af6d960bce908f0fad8bd2857b015cfe
Brad Beam <brad.beam@stormforge.io>
2021-02-17 17:00:13 -0600

    feat: Use prometheus setup task in integration test
    This changes out the postgres experiment with a dumb experiment.
    This uses the approximateRuntime to do nothing, so we dont need to
    be concerned with ci runner node resources.
    This also configures a prometheus setup task so we can validate/verify that
    it works as expected.
    
    Signed-off-by: Brad Beam <brad.beam@stormforge.io>

--- hack/integration.sh
+++ hack/integration.sh
@@ -7,20 +7,18 @@ echo "Upload image to KinD"
 [[ -n "${SETUPTOOLS_IMG}" ]] && kind load docker-image "${SETUPTOOLS_IMG}" --name chart-testing
 echo "Init redskyops"
 dist/redskyctl_linux_amd64/redskyctl init --wait
-echo "Create postgres experiment"
-kustomize build hack | kubectl apply -f -
+echo "Create ci experiment"
+kubectl apply -f hack/experiment.yaml
 echo "Create new trial"
 dist/redskyctl_linux_amd64/redskyctl generate trial \
-  --assign memory=500 \
-  --assign cpu=100 \
-  -f <(kubectl get experiment postgres-example -o yaml) | \
+  --assign something=500 \
+  --assign another=100 \
+  -f hack/experiment.yaml | \
     kubectl create -f -
 kubectl get trial -o wide
 # Change this back to a higher value when we can schedule the trial
 echo "Wait for trial to complete (300s timeout)"
 kubectl wait trial \
-  -l redskyops.dev/experiment=postgres-example \
+  -l redskyops.dev/experiment=ci \
   --for condition=redskyops.dev/trial-complete \
   --timeout 300s
-kubectl get trial -o wide
-kubectl get pods -o wide -l redskyops.dev/experiment=postgres-example
