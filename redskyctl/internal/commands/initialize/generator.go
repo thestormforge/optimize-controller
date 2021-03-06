de2e3da6078afdea40f035d3219844eec9e2d51b
Brad Beam <brad.beam@stormforge.io>
2021-03-06 11:44:45 -0600

    feat: Migrate redskyctl init to use embed.FS resources
    Signed-off-by: Brad Beam <brad.beam@stormforge.io>

--- redskyctl/internal/commands/initialize/generator.go
+++ redskyctl/internal/commands/initialize/generator.go
@@ -24,6 +24,7 @@ import (
 	"sync"
 
 	"github.com/spf13/cobra"
+	controllerYamls "github.com/thestormforge/optimize-controller/config"
 	"github.com/thestormforge/optimize-controller/internal/setup"
 	"github.com/thestormforge/optimize-controller/redskyctl/internal/commander"
 	"github.com/thestormforge/optimize-controller/redskyctl/internal/commands/authorize_cluster"
@@ -155,7 +156,7 @@ func (o *GeneratorOptions) generateApplication() (io.Reader, error) {
 	}
 
 	yamls, err := kustomize.Yamls(
-		kustomize.WithInstall(),
+		kustomize.WithEmbedResources(controllerYamls.Content),
 		kustomize.WithNamespace(ctrl.Namespace),
 		kustomize.WithImage(o.Image),
 		kustomize.WithImagePullPolicy(setup.ImagePullPolicy),
@@ -164,6 +165,7 @@ func (o *GeneratorOptions) generateApplication() (io.Reader, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	return bytes.NewReader(yamls), nil
 }
 
