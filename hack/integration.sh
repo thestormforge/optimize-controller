d49105a248c4c6680f27a4a73c42cbc4eb529c91
Brad Beam <brad.beam@stormforge.io>
2021-03-16 12:33:52 -0500

    refactor: move repeated code into a function
    This introduces a generateAndWait function for running our integration tests
    to help DRY the tests here.
    
    This also adds in some additional waits to ensure that we are in fact ready to
    start up the secondary experiment. I think we were hitting an issue with
    deleting resources associated with the first experiment ( deleting the service
    account before the trial deletion job finished ).
    
    Signed-off-by: Brad Beam <brad.beam@stormforge.io>

--- hack/integration.sh
+++ hack/integration.sh
@@ -23,60 +23,51 @@ ${REDSKYCTL_BIN} check controller --wait
 echo "Create nginx deployment"
 kubectl apply -f hack/nginx.yaml
 
-Stats() {
+stats() {
   ${REDSKYCTL_BIN} generate experiment -f hack/app.yaml
   kubectl get pods -o wide
   kubectl get trial -o wide
   kubectl logs -n redsky-system -l control-plane=controller-manager --tail=-1
 }
 
-trap Stats EXIT
+generateAndWait() {
+  echo "Create ci experiment"
+  ${REDSKYCTL_BIN} generate experiment -f ${1} | \
+    kubectl apply -f -
 
-echo "Create ci experiment"
-${REDSKYCTL_BIN} generate experiment -f hack/app.yaml | \
-  kubectl apply -f -
+  echo "Create new trial"
+  ${REDSKYCTL_BIN} generate experiment -f ${1} | \
+  ${REDSKYCTL_BIN} generate trial \
+    --default base \
+    -f - | \
+    kubectl create -f -
 
-echo "Create new trial"
-${REDSKYCTL_BIN} generate experiment -f hack/app.yaml | \
-${REDSKYCTL_BIN} generate trial \
-  --default base \
-  -f - | \
-  kubectl create -f -
+  kubectl get trial -o wide
 
-kubectl get trial -o wide
+  waitTime=300s
+  echo "Wait for trial to complete (${waitTime} timeout)"
+  kubectl wait trial \
+    -l redskyops.dev/application=ci \
+    --for condition=redskyops.dev/trial-complete \
+    --timeout ${waitTime}
 
-waitTime=300s
-echo "Wait for trial to complete (${waitTime} timeout)"
-kubectl wait trial \
-  -l redskyops.dev/application=ci \
-  --for condition=redskyops.dev/trial-complete \
-  --timeout ${waitTime}
+  echo "Wait for trial deletion tasks to finish running"
+  kubectl wait deployment \
+    redsky-default-prometheus-server \
+    --for=delete \
+    --timeout ${waitTime}
+  kubectl wait job \
+    -l redskyops.dev/trial-role=trialSetup \
+    --for condition=complete \
+    --timeout ${waitTime}
 
-echo "Remove default experiment"
-${REDSKYCTL_BIN} generate experiment -f hack/app.yaml | \
-  kubectl delete -f -
+  echo "Remove default experiment"
+  ${REDSKYCTL_BIN} generate experiment -f ${1} | \
+    kubectl delete -f -
+}
 
-# =======
-# Wait for all resources to clean up
-sleep 30
-# =======
+trap stats EXIT
 
-echo "Create ci experiment"
-${REDSKYCTL_BIN} generate experiment -f hack/app_kube.yaml | \
-  kubectl apply -f -
+generateAndWait hack/app.yaml
+generateAndWait hack/app_kube.yaml
 
-echo "Create new trial"
-${REDSKYCTL_BIN} generate experiment -f hack/app_kube.yaml | \
-${REDSKYCTL_BIN} generate trial \
-  --default base \
-  -f - | \
-  kubectl create -f -
-
-kubectl get trial -o wide
-
-waitTime=300s
-echo "Wait for trial to complete (${waitTime} timeout)"
-kubectl wait trial \
-  -l redskyops.dev/application=ci \
-  --for condition=redskyops.dev/trial-complete \
-  --timeout ${waitTime}
