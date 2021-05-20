bf972ab57052adaadaeb21565c86f0d43130aa67
Jeremy Gustie <jeremy@stormforge.io>
2021-05-20 10:19:42 -0400

    Add a field path parser to replace types.FieldPath
    Currently we are using sigs.k8s.io/kustomize/api/types.FieldPath from Kustomize to handle this. This mimics that behavior and adds the ability to treat paths as Go templates

--- internal/sfio/fieldpath.go
+++ internal/sfio/fieldpath.go
@@ -16,7 +16,45 @@ limitations under the License.
 
 package sfio
 
-import "sigs.k8s.io/kustomize/kyaml/yaml"
+import (
+	"strings"
+	"text/template"
+
+	"sigs.k8s.io/kustomize/kyaml/yaml"
+)
+
+// FieldPath evaluates a path template using the supplied context and then
+// splits it into individual path segments (honoring escaped slashes).
+func FieldPath(p string, data map[string]string) ([]string, error) {
+	// Evaluate the path as a Go Template
+	t, err := template.New("path").
+		Delims("{", "}").
+		Option("missingkey=zero").
+		Parse(p)
+	if err != nil {
+		return nil, err
+	}
+	var pathBuf strings.Builder
+	if err := t.Execute(&pathBuf, data); err != nil {
+		return nil, err
+	}
+
+	// Remove the leading slash to prevent empty elements
+	path := strings.TrimLeft(pathBuf.String(), "/")
+	if path == "" {
+		return nil, nil
+	} else if !strings.Contains(path, `\/`) {
+		return strings.Split(path, "/"), nil
+	}
+
+	// Handle escaped slashes using a temporary placeholder
+	// NOTE: This just mimics the logic of the equivalent code in the Kustomize FieldPath
+	var result []string
+	for _, pp := range strings.Split(strings.ReplaceAll(path, `\/`, `???`), "/") {
+		result = append(result, strings.ReplaceAll(pp, `???`, `/`))
+	}
+	return result, nil
+}
 
 // PreserveFieldMatcherPath wraps a field matcher so the resulting nodes have
 // their field path set accordingly (incoming path + field name).
