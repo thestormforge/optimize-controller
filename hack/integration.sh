453dc0234cb54c25dbd2134225de49a6313f0474
Jeremy Gustie <jeremy@stormforge.io>
2022-05-05 16:40:46 -0400

    Handle GoReleaser amd_v1

--- hack/integration.sh
+++ hack/integration.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-CLI_BIN="${CLI_BIN:-dist/stormforge_linux_amd64/stormforge}"
+CLI_BIN="${CLI_BIN:-dist/stormforge_linux_amd64_v1/stormforge}"
 #${CLI_CONFIG-...} allows us to test just for parameter existance which means we can rely on
 # the default and overwrite as needed ( for those pesky cases where you want to do
 # CLI_CONFIG="--stormforgeconfig /dev/null" hack/integration.sh
