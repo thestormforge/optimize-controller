676bd50be3d55d07d20ebe2d29c7a6cdc3003d20
Jeremy Gustie <jeremy@stormforge.io>
2022-10-07 09:49:39 -0400

    Fix path to executable

--- hack/integration.sh
+++ hack/integration.sh
@@ -3,7 +3,7 @@
 set -ex
 
 make cli
-CLI_BIN="${CLI_BIN:-dist/stormforge_$(go env GOOS)_amd64_v1/stormforge}"
+CLI_BIN="${CLI_BIN:-dist/optimize-controller_$(go env GOOS)_amd64_v1/stormforge}"
 #${CLI_CONFIG-...} allows us to test just for parameter existance which means we can rely on
 # the default and overwrite as needed ( for those pesky cases where you want to do
 # CLI_CONFIG="--stormforgeconfig /dev/null" hack/integration.sh
